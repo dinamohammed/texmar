8205775a57da98b5d95d95c666b0b8b5d43cabef
dinamohammed <dina.mohammed392@gmail.com>
2020-07-26 12:24:47 +0000

    so_po_cancel Module

---
+++ so_po_cancel/controllers/controllers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+
+# class SoPoCancel(2)(http.Controller):
+#     @http.route('/so_po_cancel(2)/so_po_cancel(2)/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/so_po_cancel(2)/so_po_cancel(2)/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('so_po_cancel(2).listing', {
+#             'root': '/so_po_cancel(2)/so_po_cancel(2)',
+#             'objects': http.request.env['so_po_cancel(2).so_po_cancel(2)'].search([]),
+#         })
+
+#     @http.route('/so_po_cancel(2)/so_po_cancel(2)/objects/<model("so_po_cancel(2).so_po_cancel(2)"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('so_po_cancel(2).object', {
+#             'object': obj
+#         })
+
