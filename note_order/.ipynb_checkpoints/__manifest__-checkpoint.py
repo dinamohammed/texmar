ec262f4a6a3c94fe7ce5735f80ebd0802dab176b
dinamohammed <dina.mohammed392@gmail.com>
2020-08-06 10:32:32 +0000

    Create Note order creen Module - Initial

---
+++ note_order/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Note Order",
+
+    'summary': """
+        Create a new phase of Sale order called Note order""",
+
+    'description': """
+        Create a new phase of Sale order called Note order accordeing to client requirements
+        to fulfil needs in POS, for casheir and customer.
+    """,
+
+    'author': "Egymentors",
+    'website': "http://www.egymentors.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','sale_management'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/note_order_view.xml',
+#         'views/note_order_line_view.xml',
+        'views/templates.xml',
+    ],
+}
