6c900375824698e0067102b92af98ee3e1532192
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-08-26 09:51:56 +0000

    Sell & Request Added

--- sol_fields/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ sol_fields/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'sale_stock'],
+    'depends': ['base', 'sale', 'sale_stock', 'sale_management'],
 
     # always loaded
     'data': [
