5f00c670b1ad5c80e8a1e900c4bea2ba5223a0e6
dinamohammed <dina.mohammed392@gmail.com>
2020-08-26 14:36:43 +0000

    Add Function to prepare sale order

--- sequence/models/.ipynb_checkpoints/models-checkpoint.py
+++ sequence/models/.ipynb_checkpoints/models-checkpoint.py
@@ -23,7 +23,51 @@ class SaleOrder(models.Model):
         else:
             state = 'draft'
         return state
-
+    
+#     def _prepare_so(self):
+#         self.ensure_one()
+#         return {'name' : self.client_order_ref,
+#                 'partner_id' : self.partner_id.id,
+#                 'partner_invoice_id' : self.partner_invoice_id.id,
+#                 'customer_order' : self.customer_order,
+#                 'partner_shipping_id' : self.partner_shipping_id.id,
+#                 'partner_invoice_id' : self.partner_invoice_id.id,
+#                 'sale_order_template_id' : self.sale_order_template_id.id,
+#                 'validity_date' : self.validity_date,
+#                 'date_order' : self.date_order,
+#                 'hide_cancel' : self.hide_cancel,
+#                 'stop_cancel_at' : self.stop_cancel_at,
+#                 'pricelist_id' : self.pricelist_id.id,
+#                 'print_image' : self.print_image,
+#                 'image_sizes' : self.image_sizes,
+#                 'payment_term_id' : self.payment_term_id.id,
+#                 'discount_type' : self.discount_type,
+#                 'discount_rate' : self.discount_rate,
+#                 'note' : self.note,
+#                 'amount_untaxed' : self.amount_untaxed,
+#                 'amount_discount' : self.amount_discount,
+#                 'amount_tax' : self.amount_tax,
+#                 'amount_total' : self.amount_total,
+#                 'user_id' : self.user_id.id,
+#                 'team_id' : self.team_id.id,
+#                 'company_id' : self.company_id.id,
+#                 'require_signature' : self.require_signature,
+#                 'require_payment' : self.require_payment,
+#                 'fiscal_position_id' : self.fiscal_position_id.id,
+#                 'analytic_account_id' : self.analytic_account_id.id,
+#                 'warehouse_id' : self.warehouse_id.id,
+#                 'intercom' : self.intercom.id,
+#                 'picking_policy' : self.picking_policy,
+#                 'commitment_date' : self.commitment_date,
+#                 'origin' : self.origin,
+#                 'campaign_id' : self.campaign_id.id,
+#                 'medium_id' : self.medium_id.id,
+#                 'source_id' : self.source_id.id,
+#                 'specs_conditions' : self.specs_conditions,
+#                 'signed_by' : self.signed_by,
+#                 'signed_on' : self.signed_on,
+#                 'signature' : self.signature,
+#                }
     
     
     @api.model
