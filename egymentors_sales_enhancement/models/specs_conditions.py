a3480b80da68df99d36c466ba1d1475a23a06ef2
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-09-10 15:11:10 +0000

    Only Treatment Remaining

--- egymentors_sales_enhancement/models/specs_conditions.py
+++ egymentors_sales_enhancement/models/specs_conditions.py
@@ -15,61 +15,53 @@ class SpecsConditions(models.Model):
     
     #TEMPLATE 1
     specs_conditions_1 = fields.Html(string='Template 1', required=True, default="""
-                                    <p><b>Offer Specifications and Conditions :-</b></p>
                                     <ul>
-                                    <li><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
-                                    <li>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
-                                    <li>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
-                                    <li>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
-                                    <li>Fabric specifications for indoor use only</li>
-                                    <li>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
-                                    <li>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
-                                    </ul>
-                                    <p>Ordered qty subject for (5% + or -) on delivery</p>""")
+                                    <li style='font-size:20px;'><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
+                                    <li style='font-size:20px;'>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
+                                    <li style='font-size:20px;'>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
+                                    <li style='font-size:20px;'>Fabric specifications for indoor use only</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
+                                    <li style='font-size:20px;'>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
+                                    <p style='font-size:20px;'>Ordered qty subject for (5% + or -) on delivery</p>""")
     
     
     #TEMPLATE 2
-    specs_conditions_2 = fields.Html(string='Template 1', required=True, default="""
-                                    <p><b>Offer Specifications and Conditions :-</b></p>
+    specs_conditions_2 = fields.Html(string='Template 2', required=True, default="""
                                     <ul>
-                                    <li><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
-                                    <li>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
-                                    <li>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
-                                    <li>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
-                                    <li>Fabric specifications for indoor use only</li>
-                                    <li>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
-                                    <li>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
-                                    </ul>
-                                    <p>Ordered qty subject for (5% + or -) on delivery</p>""")
+                                    <li style='font-size:20px;'><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
+                                    <li style='font-size:20px;'>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
+                                    <li style='font-size:20px;'>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
+                                    <li style='font-size:20px;'>Fabric specifications for indoor use only</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
+                                    <li style='font-size:20px;'>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
+                                    <p style='font-size:20px;'>Ordered qty subject for (5% + or -) on delivery</p>""")
     
 
     
     #TEMPLATE 3
-    specs_conditions_3 = fields.Html(string='Template 1', required=True, default="""
-                                    <p><b>Offer Specifications and Conditions :-</b></p>
+    specs_conditions_3 = fields.Html(string='Template 3', required=True, default="""
                                     <ul>
-                                    <li><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
-                                    <li>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
-                                    <li>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
-                                    <li>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
-                                    <li>Fabric specifications for indoor use only</li>
-                                    <li>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
-                                    <li>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
-                                    </ul>
-                                    <p>Ordered qty subject for (5% + or -) on delivery</p>""")
+                                    <li style='font-size:20px;'><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
+                                    <li style='font-size:20px;'>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
+                                    <li style='font-size:20px;'>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
+                                    <li style='font-size:20px;'>Fabric specifications for indoor use only</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
+                                    <li style='font-size:20px;'>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
+                                    <p style='font-size:20px;'>Ordered qty subject for (5% + or -) on delivery</p>""")
     
     
     
     #TEMPLATE 4
-    specs_conditions_4 = fields.Html(string='Template 1', required=True, default="""
-                                    <p><b>Offer Specifications and Conditions :-</b></p>
+    specs_conditions_4 = fields.Html(string='Template 4', required=True, default="""
                                     <ul>
-                                    <li><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
-                                    <li>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
-                                    <li>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
-                                    <li>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
-                                    <li>Fabric specifications for indoor use only</li>
-                                    <li>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
-                                    <li>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
-                                    </ul>
-                                    <p>Ordered qty subject for (5% + or -) on delivery</p>""")
+                                    <li style='font-size:20px;'><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
+                                    <li style='font-size:20px;'>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
+                                    <li style='font-size:20px;'>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
+                                    <li style='font-size:20px;'>Fabric specifications for indoor use only</li>
+                                    <li style='font-size:20px;'>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
+                                    <li style='font-size:20px;'>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
+                                    <p style='font-size:20px;'>Ordered qty subject for (5% + or -) on delivery</p>""")
