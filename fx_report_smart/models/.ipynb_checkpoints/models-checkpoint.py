b9620efe5eedda9ca6d2ddd3f743a5955a599a25
dinamohammed <dina.mohammed392@gmail.com>
2020-09-15 13:58:52 +0000

    Nothing else added

--- fx_report_smart/models/.ipynb_checkpoints/models-checkpoint.py
+++ fx_report_smart/models/.ipynb_checkpoints/models-checkpoint.py
@@ -29,4 +29,37 @@ class FxReport(models.AbstractModel):
         return templates
     
     
+    def _get_report_name(self):
+        return _('FX Report')
+    
+    def get_header(self, options):
+        start_date = format_date(self.env, options['date']['date_from'])
+        end_date = format_date(self.env, options['date']['date_to'])
+        return [
+            [
+                {'name': ''},
+                {'name': _('Caracteristics'), 'colspan': 4},
+                {'name': _('Assets'), 'colspan': 4},
+                {'name': _('Depreciation'), 'colspan': 4},
+                {'name': _('Book Value')},
+            ],
+            [
+                {'name': ''},  # Description
+                {'name': _('Acquisition Date'), 'class': 'text-center'},  # Caracteristics
+                {'name': _('First Depreciation'), 'class': 'text-center'},
+                {'name': _('Method'), 'class': 'text-center'},
+                {'name': _('Rate'), 'class': 'number', 'title': _('In percent.<br>For a linear method, the depreciation rate is computed per year.<br>For a degressive method, it is the degressive factor'), 'data-toggle': 'tooltip'},
+                {'name': start_date, 'class': 'number'},  # Assets
+                {'name': _('+'), 'class': 'number'},
+                {'name': _('-'), 'class': 'number'},
+                {'name': end_date, 'class': 'number'},
+                {'name': start_date, 'class': 'number'},  # Depreciation
+                {'name': _('+'), 'class': 'number'},
+                {'name': _('-'), 'class': 'number'},
+                {'name': end_date, 'class': 'number'},
+                {'name': '', 'class': 'number'},  # Gross
+            ],
+        ]
+    
+    
     
