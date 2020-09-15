ecfba2d7d09d13a3c8aa64e8de78fb0a5e76607a
dinamohammed <dina.mohammed392@gmail.com>
2020-09-15 13:41:12 +0000

    Nothing else added

--- fx_report_smart/models/.ipynb_checkpoints/models-checkpoint.py
+++ fx_report_smart/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, api, _, _lt, fields
+from odoo.tools.misc import format_date
+from datetime import timedelta
 
 
-class FxReport(models.Model):
+class FxReport(models.AbstractModel):
     _name = 'fx.report.smart'
-#     _description = 'fx_report_smart.fx_report_smart'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    _inherit = 'account.report'
+    _description = 'Fx Report'
+    
+    filter_date = {'mode': 'range', 'filter': 'this_year'}
+    filter_all_entries = False
+    filter_unfold_all = False
+    filter_account_type = [
+        {'id': 'receivable', 'name': _lt('Receivable'), 'selected': False},
+        {'id': 'payable', 'name': _lt('Payable'), 'selected': False},
+    ]
+    filter_unreconciled = False
+    filter_partner = True
+    
+    
+    
+    @api.model
+    def _get_templates(self):
+        templates = super(FxReport, self)._get_templates()
+        templates['line_template'] = 'account_reports.line_template_partner_ledger_report'
+        return templates
+    
+    
+    
