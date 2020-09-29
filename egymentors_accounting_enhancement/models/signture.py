cedf175a1243c7c7b90a929781a97d38ad30d8ba
ahmed <ahmed.salama1679@gmail.com>
2020-09-30 00:44:49 +0200

    [CHANGE] needed modification for invoice/bill print , remove unneeded module bill_invoice_layout ----> egymentors_accounting_enhancement

--- egymentors_accounting_enhancement/models/signture.py
+++ egymentors_accounting_enhancement/models/signture.py
@@ -1,9 +1,7 @@
-from odoo import models, api, _, _lt, fields
-from odoo.tools.misc import format_date
-from odoo.exceptions import ValidationError
-from datetime import timedelta
+from odoo import models, api, fields
 
-class customer(models.Model):
+
+class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
     
     name_sig = fields.Boolean('Name Signture',default=True)
@@ -11,20 +9,20 @@ class customer(models.Model):
     signture_sig = fields.Boolean('Signture',default=True)
     accounting_sig = fields.Boolean('Accounting Signture',default=True)
     ledger_sig = fields.Boolean('Genral Ledger Signture',default=True)
-    
-    
-class total_tax(models.Model):
+
+
+class AccountMoveLineInherit(models.Model):
     _inherit = 'account.move.line'
-    total_with_tax=fields.Float(compute='_calc_total_tax', store = True)
     
-    @api.depends('tax_ids','price_subtotal')
+    total_with_tax = fields.Float(compute='_calc_total_tax', store=True)
+    
+    @api.depends('tax_ids', 'price_subtotal')
     def _calc_total_tax(self):
         for line in self:
-            sum=0
+            line_sum = 0
             for tax in line.tax_ids:
-                sum+=tax.amount
-            if sum==0:
-                line.total_with_tax=0
+                line_sum += tax.amount
+            if line_sum == 0:
+                line.total_with_tax = 0
             else:
-                line.total_with_tax= (sum/100)*line.price_subtotal + line.price_subtotal    
-    
+                line.total_with_tax = (line_sum/100)*line.price_subtotal + line.price_subtotal
