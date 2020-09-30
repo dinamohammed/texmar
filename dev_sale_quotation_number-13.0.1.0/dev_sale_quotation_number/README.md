89cc0f5cb5ebe3850144f98a444fae6f63d5918c
ahmed <ahmed.salama1679@gmail.com>
2020-09-30 22:38:34 +0200

    [FIX] issues on sales module

--- dev_sale_quotation_number-13.0.1.0/dev_sale_quotation_number/README.md
+++
@@ -1,6 +0,0 @@
-Sale Quotation Number:
-=========================================================
-
-Go to Setting / apps and search "Sale Quotation Number" and Install
-
-And, you are done with installation. Congratulations!
--- dev_sale_quotation_number/model/sale_order.py
+++ dev_sale_quotation_number/model/sale_order.py
@@ -7,24 +7,23 @@
 #    For Module Support : devintelle@gmail.com  or Skype : devintelle 
 #
 ##############################################################################
-#
 from odoo import models, api, fields, _
 
-class sale_order(models.Model):
+
+class SaleOrderInherit(models.Model):
     _inherit = "sale.order"
     
     @api.model
     def _get_quotation_sequence(self):
-        seq_id = self.env['ir.sequence'].search([('code','=','sale.order')],limit=1)
+        seq_id = self.env['ir.sequence'].search([('code', '=', 'sale.order')], limit=1)
         if seq_id:
             return seq_id.id
         else:
             return False
 
-    sale_number = fields.Many2one('ir.sequence', string='Sequence',required=True,
+    sale_number = fields.Many2one('ir.sequence', string='Sequence', required=True,
                                   domain=[('code', 'in', ['sale.order', 'sale.quotation'])],
                                   default=_get_quotation_sequence)
-
 
     @api.model
     def create(self, vals):
@@ -37,19 +36,15 @@ class sale_order(models.Model):
                 else:
                     vals['name'] = self.env['ir.sequence'].next_by_code(
                         'sale.quotation') or '/'
-        return super(sale_order, self).create(vals)
-
+        return super(SaleOrderInherit, self).create(vals)
 
     def _action_confirm(self):
-        super(sale_order, self)._action_confirm()
-        sale_order_sequence_id = self.env['ir.sequence']\
-            .search([('code', '=', 'sale.order')])
+        super(SaleOrderInherit, self)._action_confirm()
+        seq_obj = self.env['ir.sequence']
+        sale_order_sequence_id = seq_obj.search([('code', '=', 'sale.order')])
         if sale_order_sequence_id:
-            if self.sale_number.id != sale_order_sequence_id.id:
-                self.sale_number = sale_order_sequence_id.id
-                self.name = self.env['ir.sequence'].next_by_code('sale.order')
-    
-    
-        
+            for order in self:
+                if order.sale_number.id != sale_order_sequence_id.id:
+                    order.sale_number = sale_order_sequence_id.id
+                    order.name = seq_obj.next_by_code('sale.order')
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
