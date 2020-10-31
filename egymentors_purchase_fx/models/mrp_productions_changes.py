900eba21f3b3c89928c8aa9812bfb05aa4277ebb
hatem1055 <55883327+hatem1055@users.noreply.github.com>
2020-10-31 21:20:37 +0200

    Update mrp_productions_changes.py

--- egymentors_purchase_fx/models/mrp_productions_changes.py
+++ egymentors_purchase_fx/models/mrp_productions_changes.py
@@ -9,19 +9,19 @@ class MrpProductionInherit(models.Model):
 	
 	sale_line_id = fields.Many2one('sale.order.line', "Sale Line")
 	
-	@api.model
-	def create(self, values):
-		raise ValidationError('ssa')
-		if values.get('origin'):
-			so_name = '/' in values.get('origin') and values.get('origin').split('/')[0] or values.get('origin')
-			order_id = self.env['sale.order'].search([('name', '=', so_name)])
-			if order_id:
-				line_id = order_id.order_line.\
-					filtered(lambda l: l.product_id.id == values.get('product_id')
-				                      and l.product_uom_qty == values.get('product_qty'))
-				values['sale_line_id'] = line_id and line_id[0].id or False
-		production = super(MrpProductionInherit, self).create(values)
-		return production
+# 	@api.model
+# 	def create(self, values):
+# 		raise ValidationError('ssa')
+# 		if values.get('origin'):
+# 			so_name = '/' in values.get('origin') and values.get('origin').split('/')[0] or values.get('origin')
+# 			order_id = self.env['sale.order'].search([('name', '=', so_name)])
+# 			if order_id:
+# 				line_id = order_id.order_line.\
+# 					filtered(lambda l: l.product_id.id == values.get('product_id')
+# 				                      and l.product_uom_qty == values.get('product_qty'))
+# 				values['sale_line_id'] = line_id and line_id[0].id or False
+# 		production = super(MrpProductionInherit, self).create(values)
+# 		return production
 	
 	def _action_cancel(self):
 		res = super(MrpProductionInherit, self)._action_cancel()
