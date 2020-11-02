2b5c25c9d13926895b95adfafb4b1b506c4222c3
hatem1055 <55883327+hatem1055@users.noreply.github.com>
2020-11-03 01:57:53 +0200

    Update mobile_number.py

--- mobile_number/models/mobile_number.py
+++ mobile_number/models/mobile_number.py
@@ -20,7 +20,7 @@ class ResPartnerInherit(models.Model):
             mobile_trimed_space = self.mobile.replace(" ", "")
             print("Number withou space: ", mobile_trimed_space, len(mobile_trimed_space))
             if len(mobile_trimed_space) != 11:
-                raise ValidationError(_("WARNING: Please Enter a Valid 11 Digit Mowwobile Number"))
+                raise ValidationError(_("WARNING: Please Enter a Valid 11 Digit Mobile Number"))
             digits = [' ', '+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
             for i in list(self.mobile):
                 if i not in digits:
