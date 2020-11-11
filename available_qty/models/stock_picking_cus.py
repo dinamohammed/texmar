031eee1ed3a50446587bdd329b66d6f7725c18cd
ahmed <ahmed.salama1679@gmail.com>
2020-11-11 20:43:05 +0200

    [TRY] to fix issue of maximum recursion on sale confirm

--- available_qty/models/stock_picking_cus.py
+++ available_qty/models/stock_picking_cus.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api ,_
-from odoo.exceptions import UserError, ValidationError
-from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo import models, _
 from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare, float_is_zero
+
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -13,7 +13,7 @@ class StockPicking(models.Model):
         self.ensure_one()
         if not self.move_lines and not self.move_line_ids:
             raise UserError(_('Please add some items to move.'))
-
+        
         # If no lots when needed, raise error
         picking_type = self.picking_type_id
         precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
@@ -21,7 +21,7 @@ class StockPicking(models.Model):
         no_reserved_quantities = all(float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for move_line in self.move_line_ids)
         if no_reserved_quantities and no_quantities_done:
             raise UserError(_('You cannot validate a transfer if no quantites are reserved nor done. To force the transfer, switch in edit more and encode the done quantities.'))
-
+        
         if picking_type.use_create_lots or picking_type.use_existing_lots:
             lines_to_check = self.move_line_ids
             if not no_quantities_done:
@@ -29,13 +29,13 @@ class StockPicking(models.Model):
                     lambda line: float_compare(line.qty_done, 0,
                                                precision_rounding=line.product_uom_id.rounding)
                 )
-
+            
             for line in lines_to_check:
                 product = line.product_id
                 if product and product.tracking != 'none':
                     if not line.lot_name and not line.lot_id:
                         raise UserError(_('You need to supply a Lot/Serial number for product %s.') % product.display_name)
-
+        
         # Propose to use the sms mechanism the first time a delivery
         # picking is validated. Whatever the user's decision (use it or not),
         # the method button_validate is called again (except if it's cancel),
@@ -43,36 +43,36 @@ class StockPicking(models.Model):
         sms_confirmation = self._check_sms_confirmation_popup()
         if sms_confirmation:
             return sms_confirmation
-
+        
         if no_quantities_done:
             view = self.env.ref('stock.view_immediate_transfer')
             wiz = self.env['stock.immediate.transfer'].create({'pick_ids': [(4, self.id)]}).process()
-#             return {
-#                 'name': _('Immediate Transfer?'),
-#                 'type': 'ir.actions.act_window',
-#                 'view_mode': 'form',
-#                 'res_model': 'stock.immediate.transfer',
-#                 'views': [(view.id, 'form')],
-#                 'view_id': view.id,
-#                 'target': 'new',
-#                 'res_id': wiz.id,
-#                 'context': self.env.context,
-#             }
-
+        #             return {
+        #                 'name': _('Immediate Transfer?'),
+        #                 'type': 'ir.actions.act_window',
+        #                 'view_mode': 'form',
+        #                 'res_model': 'stock.immediate.transfer',
+        #                 'views': [(view.id, 'form')],
+        #                 'view_id': view.id,
+        #                 'target': 'new',
+        #                 'res_id': wiz.id,
+        #                 'context': self.env.context,
+        #             }
+        
         if self._get_overprocessed_stock_moves() and not self._context.get('skip_overprocessed_check'):
             view = self.env.ref('stock.view_overprocessed_transfer')
             wiz = self.env['stock.overprocessed.transfer'].create({'picking_id': self.id}).action_confirm()
-#             return {
-#                 'type': 'ir.actions.act_window',
-#                 'view_mode': 'form',
-#                 'res_model': 'stock.overprocessed.transfer',
-#                 'views': [(view.id, 'form')],
-#                 'view_id': view.id,
-#                 'target': 'new',
-#                 'res_id': wiz.id,
-#                 'context': self.env.context,
-#             }
-
+        #             return {
+        #                 'type': 'ir.actions.act_window',
+        #                 'view_mode': 'form',
+        #                 'res_model': 'stock.overprocessed.transfer',
+        #                 'views': [(view.id, 'form')],
+        #                 'view_id': view.id,
+        #                 'target': 'new',
+        #                 'res_id': wiz.id,
+        #                 'context': self.env.context,
+        #             }
+        
         # Check backorder should check for other barcodes
         if self._check_backorder():
             return self.action_generate_backorder_wizard()
@@ -81,13 +81,11 @@ class StockPicking(models.Model):
         return res
 
 
-
-
 class SaleOrder(models.Model):
     
     _inherit = 'sale.order'
     
     def action_validate_stock(self):
-        #self.env['stock.picking'].search([('origin','=',self.name)] ,limit=1).button_validate()
+        # self.env['stock.picking'].search([('origin','=',self.name)] ,limit=1).button_validate()
         self.picking_ids.button_validate()
         return True
