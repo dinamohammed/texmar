84aad30d432ee8d260cdf65cf652d410a635f6de
Dina <odoo@eupp13.odoo.com>
2020-12-01 12:53:08 +0000

    Solve errors

--- discount_sale_purchase/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ discount_sale_purchase/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -10,25 +10,25 @@ class SaleOrder(models.Model):
     
         ###### Here we create only Gobal Discount ##########
     
-    @api.onchange('discount_type','discount_rate','order_line')
-    def discount_amount(self):
-        self._amount_all()
-        amount_untaxed = 0
-        amount_discount = 0
-        for order in self:
-            amount_untaxed = order.amount_untaxed
-            if order.discount_type :
-                if order.discount_type == 'amount':
-                    amount_untaxed = amount_untaxed - order.discount_rate
-                    amount_discount = order.discount_rate
-                elif order.discount_type == 'percent':
-                    amount_discount = amount_untaxed*(order.discount_rate/100)
-                    amount_untaxed = amount_untaxed*((100-order.discount_rate)/100)
-                order.update({
-                    'amount_untaxed': amount_untaxed,
-                    'amount_discount': amount_discount,
-                    'amount_total': amount_untaxed + order.amount_tax,
-                })            
+#     @api.onchange('discount_type','discount_rate','order_line')
+#     def discount_amount(self):
+#         self._amount_all()
+#         amount_untaxed = 0
+#         amount_discount = 0
+#         for order in self:
+#             amount_untaxed = order.amount_untaxed
+#             if order.discount_type :
+#                 if order.discount_type == 'amount':
+#                     amount_untaxed = amount_untaxed - order.discount_rate
+#                     amount_discount = order.discount_rate
+#                 elif order.discount_type == 'percent':
+#                     amount_discount = amount_untaxed*(order.discount_rate/100)
+#                     amount_untaxed = amount_untaxed*((100-order.discount_rate)/100)
+#                 order.update({
+#                     'amount_untaxed': amount_untaxed,
+#                     'amount_discount': amount_discount,
+#                     'amount_total': amount_untaxed + order.amount_tax,
+#                 })            
                     
         ###### Here we create only Total Lines of Discount ##########
         
@@ -45,8 +45,6 @@ class SaleOrder(models.Model):
             })
     
     
-    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True,
-                                      digits=dp.get_precision('Account'), track_visibility='always')
     amount_discount_line = fields.Monetary(string='Discount Lines', store=True, readonly=True,
                                            compute='_get_discount_lines',
                                       digits=dp.get_precision('Account'), track_visibility='always')
@@ -60,19 +58,27 @@ class SaleOrder(models.Model):
                                      default='percent')
     
 ############################# ADD Amounts already available in sales module ######################
-    
+    @api.onchange('discount_type','discount_rate','order_line')
     @api.depends('order_line.price_total')
     def _amount_all(self):
         """
         Compute the total amounts of the SO.
         """
         for order in self:
-            amount_untaxed = amount_tax = 0.0
+            amount_untaxed = amount_tax = amount_discount = 0.0
             for line in order.order_line:
                 amount_untaxed += line.price_subtotal
                 amount_tax += line.price_tax
+            if order.discount_type :
+                if order.discount_type == 'amount':
+                    amount_untaxed = amount_untaxed - order.discount_rate
+                    amount_discount = order.discount_rate
+                elif order.discount_type == 'percent':
+                    amount_discount = amount_untaxed*(order.discount_rate/100)
+                    amount_untaxed = amount_untaxed*((100-order.discount_rate)/100)
             order.update({
                 'amount_untaxed': amount_untaxed,
+                'amount_discount': amount_discount,
                 'amount_tax': amount_tax,
                 'amount_total': amount_untaxed + amount_tax,
             })
@@ -84,4 +90,16 @@ class SaleOrder(models.Model):
                                  track_visibility='always')
     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
                                    track_visibility='always')
+    
+    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_amount_all',
+                                      digits=dp.get_precision('Account'), track_visibility='always')
+    
+    
+#     def _prepare_invoice(self,):
+#         invoice_vals = super(SaleOrder, self)._prepare_invoice()
+#         invoice_vals.update({
+#             'discount_type': self.discount_type,
+#             'discount_rate': self.discount_rate,
+#         })
+#         return invoice_vals
 
