7f21e24d94c602ebfd86b5ea62b93d5f5cd151c5
dinamohammed <dina.mohammed392@gmail.com>
2020-12-01 10:59:02 +0000

    [ADD] Discount total and discount per line to sale order

--- discount_sale_purchase/models/sale_order.py
+++ discount_sale_purchase/models/sale_order.py
@@ -1,46 +1,89 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
     
-    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_amount_all',
+        ###### Here we create only Gobal Discount ##########
+    
+    @api.onchange('discount_type','discount_rate','order_line')
+    def discount_amount(self):
+        self._amount_all()
+        amount_untaxed = 0
+        amount_discount = 0
+        for order in self:
+            amount_untaxed = order.amount_untaxed
+            if order.discount_type :
+                if order.discount_type == 'amount':
+                    amount_untaxed = amount_untaxed - order.discount_rate
+                    amount_discount = order.discount_rate
+                elif order.discount_type == 'percent':
+                    amount_discount = amount_untaxed*(order.discount_rate/100)
+                    amount_untaxed = amount_untaxed*((100-order.discount_rate)/100)
+        order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_discount': amount_discount,
+                'amount_total': amount_untaxed + order.amount_tax,
+            })
+            
+                    
+        ###### Here we create only Total Lines of Discount ##########
+        
+    @api.onchange('order_line')
+    def _get_discount_lines(self):
+        for order in self:
+            amount_discount_line = 0
+            for line in order.order_line:
+                if line.discount:
+                    amount = (line.product_uom_qty*line.price_unit)*(line.discount/100.0)
+                    amount_discount_line = amount_discount_line + amount
+            order.update({
+                'amount_discount_line': amount_discount_line,
+            })
+    
+    
+    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True,
                                       digits=dp.get_precision('Account'), track_visibility='always')
-    amount_discount_line = fields.Monetary(string='Discount Lines', store=True, readonly=True, compute='_amount_all',
+    amount_discount_line = fields.Monetary(string='Discount Lines', store=True, readonly=True,
+                                           compute='_get_discount_lines',
                                       digits=dp.get_precision('Account'), track_visibility='always')
-    
-#     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
-#                                    track_visibility='always')
-    
+
     discount_rate = fields.Float('Discount Rate', digits=dp.get_precision('Account'),
                                  readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
     
-#     amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
-#                                      track_visibility='always')
-#     amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
-#                                  track_visibility='always')
-#     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
-#                                    track_visibility='always')
     
     discount_type = fields.Selection([('percent', 'Percentage'), ('amount', 'Amount')], string='Discount type',
                                      readonly=True,states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
                                      default='percent')
     
-    ###### Here we create only Gobal Discount ##########
+############################# ADD Amounts already available in sales module ######################
     
-    @api.onchange('discount_type','discount_rate',)
+    @api.onchange('amount_untaxed')
+    @api.depends('order_line.price_total','amount_untaxed')
     def _amount_all(self):
-        if self.discount_type :
-            if self.discount_type == 'amount':
-                self.amount_untaxed -= self.discount_rate
-                self.amount_discount = self.discount_rate
-            elif self.discount_type == 'percent':
-                self.amount_discount = self.amount_untaxed*(self.discount_rate/100)
-                self.amount_untaxed = self.amount_untaxed*((100-self.discount_rate)/100)
-        
-        for line in self:
-            if line.discount:
-                amount = (line.product_uom_qty*line.price_unit)*(line.discount/100)
-                self.amount_discount_line += amount
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_total': amount_untaxed + amount_tax,
+            })
+
+            
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
+                                     track_visibility='always')
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
+                                 track_visibility='always')
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
+                                   track_visibility='always')
+
