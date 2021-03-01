b9bf66ef5a7c4eb2e37f7adfd3a6b697260372a2
dinamohammed <dina.mohammed392@gmail.com>
2021-03-01 13:25:32 +0200

    Update account_invoice_changes.py

--- egymentors_purchase_fx/models/account_invoice_changes.py
+++ egymentors_purchase_fx/models/account_invoice_changes.py
@@ -8,6 +8,61 @@ class AccountInvoiceInherit(models.Model):
     _inherit = 'account.move'
     
     fx_num_id = fields.Many2one(related='invoice_line_ids.fx_num_id',store=True)
+    
+    @api.onchange('purchase_vendor_bill_id', 'purchase_id')
+    def _onchange_purchase_auto_complete(self):
+        ''' Load from either an old purchase order, either an old vendor bill.
+
+        When setting a 'purchase.bill.union' in 'purchase_vendor_bill_id':
+        * If it's a vendor bill, 'invoice_vendor_bill_id' is set and the loading is done by '_onchange_invoice_vendor_bill'.
+        * If it's a purchase order, 'purchase_id' is set and this method will load lines.
+
+        /!\ All this not-stored fields must be empty at the end of this function.
+        '''
+        if self.purchase_vendor_bill_id.vendor_bill_id:
+            self.invoice_vendor_bill_id = self.purchase_vendor_bill_id.vendor_bill_id
+            self._onchange_invoice_vendor_bill()
+        elif self.purchase_vendor_bill_id.purchase_order_id:
+            self.purchase_id = self.purchase_vendor_bill_id.purchase_order_id
+        self.purchase_vendor_bill_id = False
+
+        if not self.purchase_id:
+            return
+
+        # Copy partner.
+        self.partner_id = self.purchase_id.partner_id
+        self.fiscal_position_id = self.purchase_id.fiscal_position_id
+        self.invoice_payment_term_id = self.purchase_id.payment_term_id
+        self.currency_id = self.purchase_id.currency_id
+
+        # Copy purchase lines.
+        po_lines = self.purchase_id.order_line - self.line_ids.mapped('purchase_line_id')
+        new_lines = self.env['account.move.line']
+        for line in po_lines.filtered(lambda l: not l.display_type):
+            new_line = new_lines.new(line._prepare_account_move_line(self))
+            new_line.account_id = new_line._get_computed_account()
+            new_line._onchange_price_subtotal()
+            new_lines += new_line
+        new_lines._onchange_mark_recompute_taxes()
+
+        # Compute invoice_origin.
+        origins = set(self.line_ids.mapped('purchase_line_id.order_id.name'))
+        self.invoice_origin = ','.join(list(origins))
+        
+        # Add fx_num_id
+        line_ids.write({'fx_num_id':purchase_line_id.order_id.fx_num_id})
+
+        # Compute ref.
+        refs = set(self.line_ids.mapped('purchase_line_id.order_id.partner_ref'))
+        refs = [ref for ref in refs if ref]
+        self.ref = ','.join(refs)
+
+        # Compute _invoice_payment_ref.
+        if len(refs) == 1:
+            self._invoice_payment_ref = refs[0]
+
+        self.purchase_id = False
+        self._onchange_currency()
 
     def action_print(self):
         return self.env.ref('account.account_invoices').report_action(self)
