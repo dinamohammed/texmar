df870e6fe915e3663a2dfe02a115c8de07ba7458
dinamohammed <dina.mohammed392@gmail.com>
2021-04-14 16:21:09 +0000

    change date

--- dev_stock_card_report/wizard/dev_stock_card.py
+++ dev_stock_card_report/wizard/dev_stock_card.py
@@ -154,9 +154,9 @@ class dev_stock_card(models.TransientModel):
     
     def get_date(self):
         s_date = datetime.strptime(str(self.start_date), '%Y-%m-%d %H:%M:%S').date()
-        start_date = s_date.strftime('%m-%d-%Y')
+        start_date = s_date.strftime('%d-%m-%Y')
         e_date = datetime.strptime(str(self.end_date), '%Y-%m-%d %H:%M:%S').date()
-        end_date = e_date.strftime('%m-%d-%Y')        
+        end_date = e_date.strftime('%d-%m-%Y')        
         
         data = {'start_date':start_date , 'end_date':end_date}
         return data
