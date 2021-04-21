a4d69932384454f9816bd8b3f44e0c601bf9756f
dinamohammed <dina.mohammed392@gmail.com>
2021-04-21 21:22:17 +0000

    create slaes report

--- sales_compare_report/reports/sale_compare_report.py
+++ sales_compare_report/reports/sale_compare_report.py
@@ -11,10 +11,14 @@ class AppointmentReport(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         if data['form']['product_ids']:
-            products = self.env['purchase.order.line'].search([('product_id', 'in', data['form']['product_ids'])])
+            products = self.env['product.product'].search([('id','in',data['form']['product_ids'])])
+#             purchase_lines = self.env['purchase.order.line'].read_group(
+#                 [['product_id','in',data['form']['product_ids']]],[],groupby=['product_id','order_id'],lazy=False)
+            purchase_lines = self.env['purchase.order.line'].search(
+                [('product_id','in',data['form']['product_ids'])],order='product_id')
         else:
-            products = self.env['purchase.order.line'].search([])
-#         raise ValidationError('%s'%products)
+            purchase_lines = self.env['purchase.order.line'].search([])
+#         raise ValidationError('%s'%purchase_lines[0])
         # appointment_list = []
         # for app in appointments:
         #     vals = {
@@ -23,7 +27,9 @@ class AppointmentReport(models.AbstractModel):
         #         'appointment_date': app.appointment_date
         #     }
         #     appointment_list.append(vals)
+#         raise ValidationError('%s'%products)
         return {
             'doc_model': 'purchase.order.line',
+            'purchase_lines': purchase_lines,
             'products': products,
         }
