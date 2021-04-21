ecb8c7a92ca44df5264d5a0057ae99246b368fb1
dinamohammed <dina.mohammed392@gmail.com>
2021-04-21 12:21:21 +0000

    Try to solve

--- egymentors_product_code/models/product_changes.py
+++ egymentors_product_code/models/product_changes.py
@@ -82,7 +82,6 @@ class ProductTemplateInherit(models.Model):
     category_type = fields.Selection(related='categ_id.category_type')
     
     default_code = fields.Char('Internal Reference', index=True, copy = False)
-    barcode = fields.Char('Barcode', related='product_variant_ids.barcode', readonly=False, store = True)
     # Fabric Parameters
     landry_code_id = fields.Many2one('product.landry.code', "Landry Code")
     texmar_weight = fields.Many2many('texmar.weight', 'product_weight_rel', 'product_id', 'weight_id',
@@ -147,6 +146,10 @@ class ProductTemplateInherit(models.Model):
         for template in self:
             single_product_code = False
             if template.categ_id.category_type != 'not':
+                parent_categ = ''
+                category_code = ''
+                style_code = ''
+                barcode_style_code = ''
                 if template.parent_categ and template.parent_categ.code:
                     parent_categ = template.parent_categ.code[:2]
                 else:
